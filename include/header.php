7b1670e963aed0e8f29a29516cd05856d921dbb7
Igor Cacerez <igor.cacerez@gmail.com>
2022-10-17 00:26:26 -0300

    bootstrap local

--- include/header.php
+++ include/header.php
@@ -7,10 +7,7 @@
     <title>Sistema de Login</title>
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-          crossorigin="anonymous">
+    <link href="./assets/css/bootstrap.5.2.2.min.css" rel="stylesheet">
 
     <!-- CSS personalizado -->
     <link rel="stylesheet" href="./assets/css/style.css">
